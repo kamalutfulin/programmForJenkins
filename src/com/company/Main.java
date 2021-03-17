02ac50b052a24991c7796793918c595d5f69e914
kamalutfulin <kamalutfulin@gmail.com>
2021-03-17 23:53:21 +0300

    modified 23

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Получилось !");
-        System.out.println("Получилось 23!");
+        System.out.println("Получилось 23d!");
     }
 }
